a0e37f77805aca8513bc5de46eb72026bfa2bf35
lim ho <dlagh123@gmail.com>
2021-10-22 11:18:02 +0900

    .

---
+++ ex-coordinates/main.js
@@ -0,0 +1,16 @@
+const vertical = document.querySelector('.vertical');
+const horozontal = document.querySelector('.horozontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+window.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    vertical.style.left = `${x}px`;
+    horozontal.style.top = `${y}px`;
+    target.style.left = `${x}px`
+    target.style.top = `${y}px`
+    tag.innerHTML = `${x},${y}`;
+    tag.style.left = `${x}px`
+    tag.style.top = `${y}px`
+})
