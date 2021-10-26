5e4dcee8c34a0b778379c042fcdf07f63b0c353f
lim ho <dlagh123@gmail.com>
2021-10-26 21:07:05 +0900

    .

--- 2-ex-shopping/main.js
+++ 2-ex-shopping/main.js
@@ -1,7 +1,6 @@
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer_input');
 const addBtn = document.querySelector('.footer_button');
-
 function onAdd() {
     // 1. 사용자가 입련한 텍스트를 받아온다.
     const text = input.value;
@@ -20,33 +19,45 @@ function onAdd() {
     input.focus();
 
 }
-
+let id = 0;
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item_row');
+    itemRow.setAttribute('data-id', id);
+    itemRow.innerHTML = `
+    <li class="item_row">
+                <div class="item">
+                    <span class="item_name">${text}</span>
+                    <button class="item_delete">
+                        <i class="fas fa-trash-alt" data-id=${id}></i>
+                    </button>
+                </div>
+                <div class="item_divider"></div>
+            </li>
+    `;
+    id++;
+    // const item = document.createElement('div');
+    // item.setAttribute('class', 'item');
 
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-
-    const name = document.createElement('span')
-    name.setAttribute('class', 'item_name');
-    name.innerText = text;
+    // const name = document.createElement('span')
+    // name.setAttribute('class', 'item_name');
+    // name.innerText = text;
 
 
-    const deleteBtn = document.createElement('button')
-    deleteBtn.setAttribute('class', 'item_delete');
-    deleteBtn.innerHTML = '<i class ="fas fa-trash-alt"></i>';
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    })
+    // const deleteBtn = document.createElement('button')
+    // deleteBtn.setAttribute('class', 'item_delete');
+    // deleteBtn.innerHTML = '<i class ="fas fa-trash-alt"></i>';
+    // deleteBtn.addEventListener('click', () => {
+    //     items.removeChild(itemRow);
+    // })
 
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item_divider')
+    // const itemDivider = document.createElement('div');
+    // itemDivider.setAttribute('class', 'item_divider')
 
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    // item.appendChild(name);
+    // item.appendChild(deleteBtn);
+    // itemRow.appendChild(item);
+    // itemRow.appendChild(itemDivider);
 
     return itemRow;
 
@@ -58,5 +69,12 @@ addBtn.addEventListener('click', () => {
 input.addEventListener('keypress', (event) => {
     if (event.keyCode === 13) {
         onAdd();
+    }
+})
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+    if (id) {
+        const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`);
+        toBeDeleted.remove();
     }
 })
