ff709af1ee11412792fd3a410d4ad5c106de2f6d
lim ho <dlagh123@gmail.com>
2021-10-28 17:59:55 +0900

    .

---
+++ 3-carrot/src/popup.js
@@ -0,0 +1,24 @@
+'use strict';
+
+export default class Popup {
+    constructor() {
+        this.popUp = document.querySelector('.pop-up')
+        this.popUpMessage = document.querySelector('.pop-up_message')
+        this.popUpRefresh = document.querySelector('.pop-up_refresh')
+        this.popUpRefresh.addEventListener('click', () => {
+            this.onClick && this.onClick();
+            this.hide();
+        })
+    }
+    setClickListener(onClick) {
+        this.onClick = onClick;
+    }
+    showWithText(text) {
+        this.popUpMessage.innerText = text;
+        this.popUp.classList.remove('pop-up-hide');
+    }
+    hide() {
+        this.popUp.classList.add('pop-up-hide');
+    }
+
+}
