880f94d22cf9f334e457dd7824700ccf241ae837
lim ho <dlagh123@gmail.com>
2021-10-29 12:28:32 +0900

    .

--- 3-carrot/src/game.js
+++ 3-carrot/src/game.js
@@ -1,4 +1,4 @@
-import Field from './field.js';
+import { itemType, Field } from './field.js';
 import * as sound from './sound.js';
 
 export const Reason = Object.freeze({
@@ -76,13 +76,13 @@ class Game {
     }
     onItemClick(item) {
         if (!this.started) return;
-        if (item === 'carrot') {
+        if (item === itemType.carrot) {
             this.score++;
             this.updateScoreBoard();
             if (this.score === this.carrotCount) {
                 this.stop(Reason.win);
             }
-        } else if (item === 'bug') {
+        } else if (item === itemType.bug) {
             this.stop(Reason.lose);
         }
     }
